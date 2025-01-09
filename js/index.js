ce55f07800c3b715f850ab069667590b0fc81b83
Jay Christy <jaychristy0523@gmail.com>
2025-01-09 22:54:25 +0700

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Happy birthday Sayangku Cintaku'.split('')
+const text = 'Happy Birthday Cantikku !!!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
